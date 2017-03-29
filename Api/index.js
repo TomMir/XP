891baf614c1fe3d27e9ba799177202a5b5846fd7
Riccardo Rolfini <rolfo1993@gmail.com>
2017-03-29 13:38:12 +0200

    Cambiata la porta da 80 a 3000

--- Api/index.js
+++ Api/index.js
@@ -130,6 +130,6 @@ app.post('/bikes', function (req, res, next) {
 	});
 });
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
